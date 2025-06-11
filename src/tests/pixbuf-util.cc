60b104653d5ca63f9a8256dc08d59489f9ecf084
JoneKone <joona.matias.heikkila@gmail.com>
2025-06-12 02:12:27 +0300

    Fix typos and update tests

--- src/tests/pixbuf-util.cc
+++ src/tests/pixbuf-util.cc
@@ -25,8 +25,53 @@
 #include "gtest/gtest.h"
 
 #include "pixbuf-util.h"
+#include <cstring>
 
 namespace {
+
+TEST(PixbufUtilTest, SetRectFillFillsPixels)
+{
+        constexpr gint w = 4;
+        constexpr gint h = 4;
+        GdkPixbuf *pb = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, w, h);
+        const gint stride = gdk_pixbuf_get_rowstride(pb);
+        std::memset(gdk_pixbuf_get_pixels(pb), 0, stride * h);
+
+        pixbuf_set_rect_fill(pb, 1, 1, 2, 2, 10, 20, 30, 40);
+
+        guchar *pix = gdk_pixbuf_get_pixels(pb);
+        for (gint y = 1; y < 3; y++)
+                {
+                for (gint x = 1; x < 3; x++)
+                        {
+                        guchar *p = pix + y * stride + x * 4;
+                        EXPECT_EQ(10, p[0]);
+                        EXPECT_EQ(20, p[1]);
+                        EXPECT_EQ(30, p[2]);
+                        EXPECT_EQ(40, p[3]);
+                        }
+                }
+
+        g_object_unref(pb);
+}
+
+TEST(PixbufUtilTest, PixelSetSetsPixel)
+{
+        GdkPixbuf *pb = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 2, 2);
+        const gint stride = gdk_pixbuf_get_rowstride(pb);
+        std::memset(gdk_pixbuf_get_pixels(pb), 0, stride * 2);
+
+        pixbuf_pixel_set(pb, 1, 0, 11, 22, 33, 44);
+
+        guchar *pix = gdk_pixbuf_get_pixels(pb);
+        guchar *p = pix + 0 * stride + 1 * 4;
+        EXPECT_EQ(11, p[0]);
+        EXPECT_EQ(22, p[1]);
+        EXPECT_EQ(33, p[2]);
+        EXPECT_EQ(44, p[3]);
+
+        g_object_unref(pb);
+}
 
 }  // anonymous namespace
 
